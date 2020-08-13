08f53437f0f376924792c82bbd146c9374c5ccef
Marco Antônio <marcoantonio_fp@usp.br>
2020-08-13 00:57:04 -0300

    SellerList adicionada

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
+import model.services.SellerService;
 
 public class MainViewController implements Initializable {
 
@@ -43,7 +44,10 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemSellertAction() {
-		System.out.println("menu item seller");
+		loadView("/gui/SellerList.fxml", (SellerListController controller) -> {
+			controller.setSellerService(new SellerService());
+			controller.updateTableView();
+		});
 	}	
 	
 	@FXML
