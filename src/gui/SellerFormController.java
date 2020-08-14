e1609ab903354a69ed56da4ac45a672369605a27
Marco Antônio <marcoantonio_fp@usp.br>
2020-08-14 00:59:56 -0300

    Adicionados demais campos do Seller na tela de cadastro

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,163 @@
+package gui;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable {
+
+	private SellerService service;
+	
+	private Seller entity;
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private TextField txtEmail;
+	
+	@FXML
+	private DatePicker dpBirthDate;
+	
+	@FXML
+	private TextField txtBaseSalary;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML
+	private Label labelErrorEmail;	
+
+	@FXML
+	private Label labelErrorBirthDate;	
+	
+	@FXML
+	private Label labelErrorBaseSalary;	
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel; 
+	
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) throw new IllegalStateException("Entity was null");
+		if(service == null) throw new IllegalStateException("Service was null");
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			
+			notifyDataChangeListeners();
+			
+			//fechando a janela apos salvar corretamente
+			Utils.currentStage(event).close();
+		} catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+		catch(ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
+	}
+	
+	private void notifyDataChangeListeners() {
+
+		for(DataChangeListener dl : dataChangeListeners) {
+			dl.onDataChanged();
+		}
+	}
+
+	private Seller getFormData() {
+		Seller obj = new Seller();
+	
+		ValidationException exception = new ValidationException("Validation Error");
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		//verificando se o campo nome for vazio
+		if(txtName.getText() == null || txtName.getText().trim().equals("")) exception.addError("Name", "Field cant't be empty");
+		obj.setName(txtName.getText());
+		
+		if(exception.getErrors().size() > 0) throw exception;
+		
+		return obj;
+	}
+
+	@FXML
+	public void onBtCacelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
+	}
+	
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+	
+	public void setSellerService(SellerService service){
+		this.service = service;
+	}
+	
+	//metodo responsavel por pegar o conteudo da variavel entity e popular a as caixas de texto do formulario
+	public void updateFormData() {
+		if (entity == null) throw new IllegalStateException("Entity was null");
+		txtId.setText( entity.getId() == null ? "" : String.valueOf(entity.getId()));
+		txtName.setText(entity.getName() == null ? "" : String.valueOf(entity.getName()));
+		txtEmail.setText(entity.getEmail());
+		Locale.setDefault(Locale.US);
+		txtBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
+		if(entity.getBirthDate() != null) {
+			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
+		}
+	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}	
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set <String> fields = errors.keySet();
+		if(fields.contains("Name")) labelErrorName.setText(errors.get("Name"));
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 50);
+		Constraints.setTextFieldDouble(txtBaseSalary);
+		Constraints.setTextFieldMaxLength(txtEmail, 60);
+		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
+		
+	}
+}
